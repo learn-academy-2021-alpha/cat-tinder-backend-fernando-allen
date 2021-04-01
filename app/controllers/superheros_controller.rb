de5f5c58ca9db402dbc9fd28009f1ab7a0b75b20
romerofernando <romero_f22@yahoo.com>
2021-04-01 15:07:41 -0700

    cat seeds

--- app/controllers/superheros_controller.rb
+++
@@ -1,2 +0,0 @@
-class SuperherosController < ApplicationController
-end
--- spec/requests/supers_spec.rb
+++ spec/requests/supers_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Superheros", type: :request do
+RSpec.describe "Supers", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
