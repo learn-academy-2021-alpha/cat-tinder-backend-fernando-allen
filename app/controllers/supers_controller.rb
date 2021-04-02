9f8af416cb84b3503095f06f18c38a6e0c570ccb
algtan <agtan@ymail.com>
2021-04-02 14:16:53 -0700

    api endpoint for index

--- app/controllers/supers_controller.rb
+++ app/controllers/supers_controller.rb
@@ -1,2 +1,6 @@
 class SupersController < ApplicationController
+    def index
+        supers = Super.all
+        render json: supers
+    end
 end
--- spec/requests/supers_spec.rb
+++ spec/requests/supers_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Supers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET/ supers" do 
+    it "gets all supers" do
+    Super.create(name:"hulk", power_level:100, place_of_birth:"ohio", hobbies:"meditation", image_url:"google.com")
+    get '/supers'
+    json = JSON.parse(response.body)
+    expect(response).to have_http_status(:ok)
+    puts json
+    expect(json.length).to eq 1
+
+    
+    end
   end
 end
