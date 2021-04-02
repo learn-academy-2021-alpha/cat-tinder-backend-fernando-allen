0faf2cd5c6b3a8cb69e3e149fb14c64ac51cfd86
algtan <agtan@ymail.com>
2021-04-02 14:30:11 -0700

    added show endpoint

--- app/controllers/supers_controller.rb
+++ app/controllers/supers_controller.rb
@@ -3,4 +3,18 @@ class SupersController < ApplicationController
         supers = Super.all
         render json: supers
     end
+
+    def create
+        superhero = Super.create(superhero_params)
+        if superhero.valid?
+          render json: superhero
+        else
+          render json: superhero.errors, status: 422
+        end
+    end
+
+    private
+    def superhero_params
+        params.require(:superhero).permit(:name, :power_level, :place_of_birth, :hobbies, :image_url)
+    end
 end
--- spec/requests/supers_spec.rb
+++ spec/requests/supers_spec.rb
@@ -13,4 +13,35 @@ RSpec.describe "Supers", type: :request do
     
     end
   end
+
+  describe "POST /supers" do
+    it 'creates a new super' do
+      # arrange - build request with params
+      superhero_params = {
+        superhero: {
+          name:"hawkeye",
+          power_level:1,
+          place_of_birth:"iowa",
+          hobbies:"archery",
+          image_url:"google.com"
+        }
+      }
+
+      # act - make POST request
+      post '/supers', params: superhero_params
+
+      # assert
+      # cat = Cat.first
+      # expect(cat.name).to eq 'Garfield'
+      # expect(cat.age).to eq 32
+      # expect(cat.enjoys).to eq 'silently judging you'
+
+      superhero_response = JSON.parse(response.body)
+      expect(superhero_response['name']).to eq 'hawkeye'
+      expect(superhero_response['power_level']).to eq 1
+      expect(superhero_response['place_of_birth']).to eq 'iowa'
+      expect(superhero_response['hobbies']).to eq 'archery'
+      expect(superhero_response['image_url']).to eq 'google.com'
+    end
+  end
 end
